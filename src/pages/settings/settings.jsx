1727e01c91fa71892178a337f6cd2a3f2dd7595d
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-09-29 21:51:09 +0300

    Folders and empty files created

---
+++ src/pages/settings/settings.jsx
@@ -0,0 +1,4 @@
+import styles from './settings.module.css';
+export default function Settings() {
+  return;
+}
---
+++ src/pages/statistics/stats.jsx
@@ -0,0 +1,4 @@
+import styles from "./statistics.module.css";
+export default function Statistics() {
+  return;
+}
